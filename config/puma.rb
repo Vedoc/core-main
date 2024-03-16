5e51e766c3aa2f2a18c07f53c72c54b39d2b1864
arnoldnekemiah <arnoldnek@gmail.com>
2024-03-16 19:11:36 +0300

    fixed puma file3

--- config/puma.rb
+++ config/puma.rb
@@ -15,8 +15,12 @@ port ENV.fetch('PORT') { 3000 }
 #
 environment ENV.fetch('RAILS_ENV') { 'development' }
 
-# Define the app block using Rackup::Handler::Default
-app = Rack::Handler::Default.build(Rails.application)
+# Define the app block using Rackup::Builder
+app do
+  Rack::Builder.new {
+    run Rails.application
+  }
+end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
