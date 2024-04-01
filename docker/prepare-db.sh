b1a59cecd7a8e32df5dba38c8dd45a4fb44146fc
arnoldnekemiah <arnoldnek@gmail.com>
2024-04-01 12:15:42 +0300

    uncommented prepare-db file

--- docker/prepare-db.sh
+++ docker/prepare-db.sh
@@ -1,5 +1,5 @@
 # ! /bin/sh
 
-# If database exists, migrate. Otherwise, create and seed
-# bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup db:seed
-# echo 'Database migration and seeding completed!'
+If database exists, migrate. Otherwise, create and seed
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup db:seed
+echo 'Database migration and seeding completed!'
