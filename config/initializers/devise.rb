24c07750c49f51617d68b88f693a1ff67736f5a6
arnoldnek@gmail.com <arnoldnek@gmail.com>
2024-05-30 12:06:41 +0300

    fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,5 @@
 Devise.setup do | config |
-  config.secret_key = Rails.application.credentials.devise[:secret_key]
+  # config.secret_key = Rails.application.credentials.devise[:secret_key]
+  config.secret_key = Rails.application.credentials.dig(:devise, :secret_key)
   config.mailer_sender = ENV[ 'APP_EMAIL' ]
 end
