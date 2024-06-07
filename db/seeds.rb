1b1afac0d6dced46e08e9fc70ac5128a600ebd9f
arnoldnek@gmail.com <arnoldnek@gmail.com>
2024-06-07 23:51:41 +0300

    fix 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-%w[car truck].each do |category|
-  CarCategory.create(name: category)
-end
-
-# AdminUser.find_or_create_by(email: 'admin@mail.com') do |admin|
-#   admin.password = 'password' # Replace 'password' with the actual password
+# %w[car truck].each do |category|
+#   CarCategory.create(name: category)
 # end
+
+AdminUser.find_or_create_by(email: 'admin@mail.com') do |admin|
+  admin.password = 'password' # Replace 'password' with the actual password
+end
