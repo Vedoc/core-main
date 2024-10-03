50b1d7cca316afd8e18a61c2cd49682dbc2c2453
arnoldnekemiah <arnoldnek@gmail.com>
2024-10-03 17:52:35 +0300

    added ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -68,6 +68,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = {
+    host : ENV[ 'APP_HOST' ]
+    protocol : 'https'
+  }
 
   config.action_mailer.smtp_settings = {
     user_name: ENV[ 'MAIL_USERNAME' ],
