a0ea9a52438790ea6ab0eeb92b04904def13d4e3
arnoldnekemiah <arnoldnek@gmail.com>
2024-10-09 17:26:03 +0300

    updated puma file to allow nginx connection

--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,12 @@
 # Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port ENV.fetch('PORT') { 3000 }
+# Bind to a Unix socket instead of a TCP port
+bind "unix:///home/ubuntu/core-main/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch('RAILS_ENV') { 'development' }
+environment ENV.fetch('RAILS_ENV') { 'production' }
 
 # Define the app block as a Rack application
 app do |env|
