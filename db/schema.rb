d1d2dfcbc0285f735f8608938389460d63607ac4
arnoldnekemiah <arnoldnek@gmail.com>
2025-01-22 05:07:30 +0300

    fix database schema issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_21_125223) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_22_020207) do
   create_schema "topology"
 
   # These are extensions that must be enabled in order to support this database
