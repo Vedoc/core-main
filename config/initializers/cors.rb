dedf971035dd73df73928dfc5f706d5898bc463b
arnoldnekemiah <arnoldnek@gmail.com>
2025-01-22 05:58:51 +0300

    cors file updated to allow credentials

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://vedocapp.com'
-
+    
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      credentials: true
   end
 end
