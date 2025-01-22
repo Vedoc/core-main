87f466837dc4dd112f93227e7ae2813d0a27a83d
arnoldnekemiah <arnoldnek@gmail.com>
2025-01-22 05:06:33 +0300

    fix database schema issue

--- db/migrate/20250121125223_add_devise_confirmable_to_accounts.rb
+++
@@ -1,9 +0,0 @@
-class AddDeviseConfirmableToAccounts < ActiveRecord::Migration[7.1]
-  def change
-    add_column :accounts, :confirmation_token, :string
-    add_column :accounts, :confirmation_sent_at, :datetime
-    add_column :accounts, :unconfirmed_email, :string
-
-    add_index :accounts, :confirmation_token, unique: true
-  end
-end
