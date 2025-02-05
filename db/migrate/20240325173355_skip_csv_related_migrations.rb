26ea3600383cc0a36759fefaeabd039ff4adc312
arnoldnekemiah <arnoldnek@gmail.com>
2025-02-05 22:09:26 +0300

    fixed migration error

--- db/migrate/20240325173355_skip_csv_related_migrations.rb
+++ db/migrate/20240325173355_skip_csv_related_migrations.rb
@@ -1,19 +1,21 @@
 class SkipCsvRelatedMigrations < ActiveRecord::Migration[7.1]
   def up
     migrations_to_skip = [
-      '20250125133331', # remove_string_from_vehicles
-      '20250125133332', # other problematic migrations
+      '20240125133331', # remove_string_from_vehicles
+      '20240125133332', # other problematic migrations
     ]
 
     migrations_to_skip.each do |version|
-      execute "INSERT INTO schema_migrations (version) VALUES ('#{version}')"
+      if !ActiveRecord::SchemaMigration.where(version: version).exists?
+        execute "INSERT INTO schema_migrations (version) VALUES ('#{version}')"
+      end
     end
   end
 
   def down
     migrations_to_skip = [
-      '20250125133331',
-      '20250125133332',
+      '20240125133331',
+      '20240125133332',
     ]
 
     migrations_to_skip.each do |version|
